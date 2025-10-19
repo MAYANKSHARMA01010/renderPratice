2609260c2eef0412660ecbc9b89be824b3b76f24
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-10-19 15:45:48 +0530

    data added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { json } = require('stream/consumers');
 const app = express();
 require('dotenv').config();
 
@@ -15,6 +16,84 @@ app.get('/home', (req, res) => {
 app.get('/about', (req, res) => {
     res.send('About Us Page');
 });
+
+app.get("/data",(req,res) => {
+    res.status(200).json({
+        "data": 
+            [
+                {
+                    "id": 1,
+                    "name": "John Doe",
+                    "email": "john.doe@example.com",
+                    "age": 28,
+                    "isActive": true
+                },
+                {
+                    "id": 2,
+                    "name": "Jane Smith",
+                    "email": "jane.smith@example.com",
+                    "age": 25,
+                    "isActive": false
+                },
+                {
+                    "id": 3,
+                    "name": "Michael Johnson",
+                    "email": "michael.johnson@example.com",
+                    "age": 32,
+                    "isActive": true
+                },
+                {
+                    "id": 4,
+                    "name": "Emily Davis",
+                    "email": "emily.davis@example.com",
+                    "age": 30,
+                    "isActive": true
+                },
+                {
+                    "id": 5,
+                    "name": "William Brown",
+                    "email": "william.brown@example.com",
+                    "age": 27,
+                    "isActive": false
+                },
+                {
+                    "id": 6,
+                    "name": "Olivia Wilson",
+                    "email": "olivia.wilson@example.com",
+                    "age": 24,
+                    "isActive": true
+                },
+                {
+                    "id": 7,
+                    "name": "James Taylor",
+                    "email": "james.taylor@example.com",
+                    "age": 35,
+                    "isActive": true
+                },
+                {
+                    "id": 8,
+                    "name": "Sophia Anderson",
+                    "email": "sophia.anderson@example.com",
+                    "age": 29,
+                    "isActive": false
+                },
+                {
+                    "id": 9,
+                    "name": "Daniel Thomas",
+                    "email": "daniel.thomas@example.com",
+                    "age": 31,
+                    "isActive": true
+                },
+                {
+                    "id": 10,
+                    "name": "Ava Martinez",
+                    "email": "ava.martinez@example.com",
+                    "age": 26,
+                    "isActive": true
+                }
+         ]
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
